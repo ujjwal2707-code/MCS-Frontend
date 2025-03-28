475803db6fa31b5037bcfbae1009e02aa1f043ce
ujjwal2707-code <bro2code000@gmail.com>
2025-03-28 17:42:28 +0530

    bottom tab styling

--- src/navigation/bottom-tab-navigator.tsx
+++ src/navigation/bottom-tab-navigator.tsx
@@ -3,6 +3,7 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {Home, Profile, Contact} from '../screens';
 import {Paths} from './paths';
 import {BottomTabParamList} from './bottom-tab-params';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const Tab = createBottomTabNavigator<BottomTabParamList>();
 
@@ -10,7 +11,32 @@ const BottomTabNavigator = () => {
   return (
     <Tab.Navigator
       initialRouteName={Paths.Home}
-      screenOptions={{headerShown: false}}>
+      screenOptions={({route}) => ({
+        headerShown: false,
+        tabBarIcon: ({focused, color, size}) => {
+          let iconName = '';
+
+          if (route.name === Paths.Home) {
+            iconName = focused ? 'home' : 'home-outline';
+          } else if (route.name === Paths.Profile) {
+            iconName = focused ? 'account' : 'account-outline';
+          } else if (route.name === Paths.Contact) {
+            iconName = focused ? 'phone' : 'phone-outline';
+          }
+
+          return (
+            <MaterialCommunityIcons name={iconName} size={30} color={color} />
+          );
+        },
+        tabBarActiveTintColor: 'white',
+        tabBarInactiveTintColor: 'grey',
+        tabBarStyle: {
+          backgroundColor: '#5A29FD',
+          height: 100,
+          paddingBottom: 20,
+          paddingTop: 20,
+        },
+      })}>
       <Tab.Screen name={Paths.Home} component={Home} />
       <Tab.Screen name={Paths.Profile} component={Profile} />
       <Tab.Screen name={Paths.Contact} component={Contact} />
