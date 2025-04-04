459894d72301460bfa03ec1e8db838d0a78ede2c
ujjwal2707-code <bro2code000@gmail.com>
2025-04-04 23:29:13 +0530

    scan qr code

--- src/screens/scan-qr.tsx
+++ src/screens/scan-qr.tsx
@@ -1,15 +1,12 @@
 import React, {useEffect, useRef, useState} from 'react';
 import {
   View,
-  Text,
   StyleSheet,
-  TouchableOpacity,
   Dimensions,
-  Animated,
   Button,
+  Alert,
+  Linking,
 } from 'react-native';
-import Ionicons from 'react-native-vector-icons/Ionicons';
-import {SafeAreaView} from 'react-native-safe-area-context';
 import {
   Camera,
   useCameraDevice,
@@ -17,9 +14,20 @@ import {
 } from 'react-native-vision-camera';
 import {useCodeScanner} from 'react-native-vision-camera';
 import {isValidUrl} from '../../utils/is-valid-url';
-import ScanQrDetails from '../components/scan-qr-details';
-import { RootScreenProps } from '../navigation/types';
-import { Paths } from '../navigation/paths';
+import {RootScreenProps} from '../navigation/types';
+import {Paths} from '../navigation/paths';
+import ScreenLayout from '@components/screen-layout';
+import ScreenHeader from '@components/screen-header';
+import Loader from '@components/loader';
+import CustomText from '@components/ui/custom-text';
+import BottomSheet, {BottomSheetScrollView} from '@gorhom/bottom-sheet';
+import CustomButton from '@components/ui/custom-button';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import {ScanURLResult} from 'types/types';
+import {useMutation} from '@tanstack/react-query';
+import {apiService} from '@services/index';
+import {Card} from 'react-native-paper';
+import HorizontalBarsChart from '@components/bar-chart';
 
 enum QRTypeState {
   PaymentLink = 'Payment Link',
@@ -29,29 +37,32 @@ enum QRTypeState {
   Unknown = 'Unknown',
 }
 
-// Device dimensions and scan rectangle settings.
 const {width, height} = Dimensions.get('window');
-const SCAN_RECT_WIDTH = width * 0.7;
-const SCAN_RECT_HEIGHT = 250;
-const SCAN_RECT_LEFT = (width - SCAN_RECT_WIDTH) / 2;
-const SCAN_RECT_TOP = (height - SCAN_RECT_HEIGHT) / 2;
+
+// const SCAN_RECT_WIDTH = width * 0.7;
+// const SCAN_RECT_HEIGHT = 250;
+// const SCAN_RECT_LEFT = (width - SCAN_RECT_WIDTH) / 2;
+// const SCAN_RECT_TOP = (height - SCAN_RECT_HEIGHT) / 2;
 
 const ScanQR = ({navigation}: RootScreenProps<Paths.ScanQr>) => {
   const [hasPermissions, setHasPermissions] = useState(false);
   const [scannedData, setScannedData] = useState<string | null>(null);
   const [qrType, setQRType] = useState<QRTypeState>(QRTypeState.Unknown);
   const cameraRef = useRef<Camera>(null);
-  const scanningLineAnim = useRef(new Animated.Value(0)).current;
 
-  const [modalVisible, setModalVisible] = useState(false);
+  const [openScanResult, setOpenScanResult] = useState(false);
 
-  const handleModelOpen = () => {
-    setModalVisible(true);
-  };
+  // const scanningLineAnim = useRef(new Animated.Value(0)).current;
 
-  const handleModelClose = () => {
-    setModalVisible(false);
-  };
+  // const [modalVisible, setModalVisible] = useState(false);
+
+  // const handleModelOpen = () => {
+  //   setModalVisible(true);
+  // };
+
+  // const handleModelClose = () => {
+  //   setModalVisible(false);
+  // };
 
   const {hasPermission, requestPermission} = useCameraPermission();
 
@@ -63,14 +74,15 @@ const ScanQR = ({navigation}: RootScreenProps<Paths.ScanQr>) => {
         setHasPermissions(true);
       }
     })();
-    startLineAnimation();
+    // startLineAnimation();
   }, []);
 
   const handleScannedData = (data: string) => {
     setScannedData(data);
     const detectedType = detectQRType(data);
     setQRType(detectedType);
-    handleModelOpen();
+    // handleModelOpen();
+    setOpenScanResult(true);
   };
 
   const detectQRType = (data: string): QRTypeState => {
@@ -100,30 +112,30 @@ const ScanQR = ({navigation}: RootScreenProps<Paths.ScanQr>) => {
 
   console.log(hasPermissions, scannedData, qrType);
 
-  const startLineAnimation = () => {
-    scanningLineAnim.setValue(0);
-    Animated.loop(
-      Animated.sequence([
-        Animated.timing(scanningLineAnim, {
-          toValue: SCAN_RECT_HEIGHT,
-          duration: 1000,
-          useNativeDriver: true,
-        }),
-        Animated.timing(scanningLineAnim, {
-          toValue: 0,
-          duration: 1000,
-          useNativeDriver: true,
-        }),
-      ]),
-    ).start();
-  };
+  // const startLineAnimation = () => {
+  //   scanningLineAnim.setValue(0);
+  //   Animated.loop(
+  //     Animated.sequence([
+  //       Animated.timing(scanningLineAnim, {
+  //         toValue: SCAN_RECT_HEIGHT,
+  //         duration: 1000,
+  //         useNativeDriver: true,
+  //       }),
+  //       Animated.timing(scanningLineAnim, {
+  //         toValue: 0,
+  //         duration: 1000,
+  //         useNativeDriver: true,
+  //       }),
+  //     ]),
+  //   ).start();
+  // };
 
   if (!hasPermission) {
     return (
       <View style={styles.permissionContainer}>
-        <Text style={styles.permissionText}>
+        <CustomText color="#fff" style={styles.permissionText}>
           We need your permission to use the camera
-        </Text>
+        </CustomText>
         <Button
           onPress={async () => {
             const status = await requestPermission();
@@ -138,111 +150,229 @@ const ScanQR = ({navigation}: RootScreenProps<Paths.ScanQr>) => {
   }
 
   if (device == null) {
-    return (
-      <View style={styles.permissionContainer}>
-        <Text style={styles.permissionText}>Loading camera...</Text>
-      </View>
-    );
+    return <Loader />;
   }
 
   return (
-    <SafeAreaView style={styles.container}>
-      <TouchableOpacity
-        style={styles.closeButton}
-        onPress={() => navigation.navigate(Paths.Home)}
-      >
-        <Text style={styles.closeButtonText}>
-          <Ionicons name="close" size={20} color="black" />
-        </Text>
-      </TouchableOpacity>
-      <View style={styles.cameraContainer}>
-        <Camera
-          ref={cameraRef}
-          style={StyleSheet.absoluteFill}
-          device={device}
-          isActive={true}
-          codeScanner={codeScanner}
-        />
-        {/* Overlays for the scan area */}
-        <View
-          style={[
-            styles.overlay,
-            {top: 0, left: 0, right: 0, height: SCAN_RECT_TOP},
-          ]}
-        />
-        <View
-          style={[
-            styles.overlay,
-            {
-              top: SCAN_RECT_TOP + SCAN_RECT_HEIGHT,
-              left: 0,
-              right: 0,
-              bottom: 0,
-            },
-          ]}
-        />
-        <View
-          style={[
-            styles.overlay,
-            {
-              top: SCAN_RECT_TOP,
-              left: 0,
-              width: SCAN_RECT_LEFT,
-              height: SCAN_RECT_HEIGHT,
-            },
-          ]}
-        />
-        <View
-          style={[
-            styles.overlay,
-            {
-              top: SCAN_RECT_TOP,
-              left: SCAN_RECT_LEFT + SCAN_RECT_WIDTH,
-              right: 0,
-              height: SCAN_RECT_HEIGHT,
-            },
-          ]}
-        />
-        <View
-          style={[
-            styles.scanRect,
-            {
-              left: SCAN_RECT_LEFT,
-              top: SCAN_RECT_TOP,
-              width: SCAN_RECT_WIDTH,
-              height: SCAN_RECT_HEIGHT,
-            },
-          ]}>
-          <View style={styles.scanLineContainer}>
-            <Animated.View
-              style={[
-                styles.scanLine,
-                {transform: [{translateY: scanningLineAnim}]},
-              ]}
-            />
-          </View>
+    <ScreenLayout>
+      <ScreenHeader name="QR Code Scanner" />
+      <View style={styles.scannerContainer}>
+        <View style={styles.scannerInnerContainer}>
+          <Camera
+            ref={cameraRef}
+            style={StyleSheet.absoluteFill}
+            device={device}
+            isActive={true}
+            codeScanner={codeScanner}
+          />
         </View>
       </View>
       {scannedData && (
-        <ScanQrDetails
-          modalVisible={modalVisible}
-          setModalVisible={setModalVisible}
-          handleModelClose={handleModelClose}
+        <ScanQRResult
+          isOpen={openScanResult}
+          onClose={() => setOpenScanResult(false)}
           scannedData={scannedData}
           qrType={qrType}
         />
       )}
-    </SafeAreaView>
+    </ScreenLayout>
+  );
+};
+
+const SCAN_SIZE = width * 0.8;
+
+export default ScanQR;
+
+// ScanQRResult Component
+interface ScanQRResultProps {
+  isOpen: boolean;
+  onClose: () => void;
+  scannedData: string;
+  qrType: string;
+}
+const ScanQRResult = ({
+  isOpen,
+  onClose,
+  scannedData,
+  qrType,
+}: ScanQRResultProps) => {
+  const bottomSheetRef = useRef<BottomSheet>(null);
+
+  const [scanUrlDetails, setScanUrlDetails] = useState<ScanURLResult | null>(
+    null,
+  );
+
+  const {mutateAsync: scanUriMutation, isPending: scanUriMutationPending} =
+    useMutation({
+      mutationFn: (values: {inputUrl: string}) => apiService.scanUri(values),
+      onSuccess: res => {
+        setScanUrlDetails(res.data);
+      },
+      onError: (err: any) => {
+        Alert.alert('Error', err);
+      },
+    });
+
+  const handleScanURL = async () => {
+    try {
+      await scanUriMutation({inputUrl: scannedData});
+    } catch (error) {}
+  };
+
+  useEffect(() => {
+    if (isOpen) {
+      bottomSheetRef.current?.snapToIndex(0);
+    }
+  }, [isOpen]);
+
+  if (!isOpen) return null;
+  return (
+    <BottomSheet
+      ref={bottomSheetRef}
+      snapPoints={['45%', '90%']}
+      index={1}
+      enablePanDownToClose={true} // Allows swipe down to close
+      onClose={onClose}
+      backgroundStyle={{backgroundColor: '#4E4E96'}}>
+      <BottomSheetScrollView style={{flex: 1, paddingHorizontal: 20}}>
+        <View style={styles.infoBox}>
+          <Ionicons
+            name="information-circle-sharp"
+            size={24}
+            color="#fff"
+            style={styles.infoIcon}
+          />
+          {qrType === 'Payment Link' ? (
+            <CustomText
+              variant="h5"
+              color="#fff"
+              fontFamily="Montserrat-SemiBold">
+              This QR is a {qrType}. Verify sender before proceeding.
+            </CustomText>
+          ) : (
+            <CustomText
+              variant="h5"
+              color="#fff"
+              fontFamily="Montserrat-SemiBold">
+              This QR is a {qrType}.
+            </CustomText>
+          )}
+        </View>
+
+        <View style={{paddingVertical: 5}}>
+          <CustomText
+            variant="h5"
+            color="#fff"
+            fontFamily="Montserrat-SemiBold"
+            fontSize={18}
+            style={{textAlign: 'center'}}>
+            {scannedData}
+          </CustomText>
+        </View>
+
+        {qrType === 'Link' && (
+          <CustomButton
+            title="Scan URL"
+            style={{width: '50%', alignSelf: 'center'}}
+            onPress={handleScanURL}
+            isLoading={scanUriMutationPending}
+            isDisabled={!scannedData}
+          />
+        )}
+
+        {qrType === 'Payment Link' && (
+          <CustomButton
+            title="Scan URL"
+            style={{width: '50%', alignSelf: 'center'}}
+            onPress={handleScanURL}
+            isLoading={scanUriMutationPending}
+            isDisabled={!scannedData}
+          />
+        )}
+
+        {/** Scan Result Analysis */}
+
+        {scanUrlDetails && (
+          <>
+            {scanUrlDetails &&
+              (scanUrlDetails.stats.harmless +
+                scanUrlDetails.stats.undetected >=
+              10 *
+                (scanUrlDetails.stats.malicious +
+                  scanUrlDetails.stats.suspicious) ? (
+                <Card style={styles.harmlessCard}>
+                  <Card.Content>
+                    <View>
+                      <CustomText
+                        variant="h5"
+                        color="#fff"
+                        fontFamily="Montserrat-Bold"
+                        fontSize={18}
+                        style={{textAlign: 'center'}}>
+                        Safe & Harmless Link
+                      </CustomText>
+                      <CustomText
+                        variant="h5"
+                        color="#fff"
+                        fontFamily="Montserrat-Bold"
+                        style={{textAlign: 'center'}}>
+                        {scanUrlDetails.stats.malicious} malicious threats were
+                        detected on this website. It is considered safe by most
+                        security checks.
+                      </CustomText>
+                    </View>
+                  </Card.Content>
+                </Card>
+              ) : (
+                <Card style={styles.maliciousCard}>
+                  <Card.Content>
+                    <View>
+                      <CustomText
+                        variant="h5"
+                        color="#fff"
+                        fontFamily="Montserrat-Bold"
+                        fontSize={18}
+                        style={{textAlign: 'center'}}>
+                        Suspected Fraud or Malicious Link
+                      </CustomText>
+                      <CustomText
+                        variant="h5"
+                        color="#fff"
+                        fontFamily="Montserrat-Bold"
+                        style={{textAlign: 'center'}}>
+                        {scanUrlDetails.stats.malicious} malicious threats were
+                        detected on this website. This website has been flagged
+                        as malicious or suspicious by multiple sources. Please
+                        proceed with caution.
+                      </CustomText>
+                    </View>
+                  </Card.Content>
+                </Card>
+              ))}
+
+            <Card style={styles.chartContainer}>
+              <Card.Content>
+                <HorizontalBarsChart stats={scanUrlDetails?.stats!} />
+              </Card.Content>
+            </Card>
+
+            <CustomButton
+              title="Open Link"
+              onPress={() => {
+                if (scanUrlDetails) {
+                  Linking.openURL(scanUrlDetails.meta.url_info.url);
+                }
+              }}
+              style={{marginTop: 10, marginBottom: 10}}
+            />
+          </>
+        )}
+      </BottomSheetScrollView>
+    </BottomSheet>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  cameraContainer: {
-    flex: 1,
-  },
   permissionContainer: {
     flex: 1,
     justifyContent: 'center',
@@ -252,43 +382,51 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     marginBottom: 10,
   },
-  closeButton: {
-    position: 'absolute',
-    top: 20,
-    right: 20,
-    backgroundColor: 'gray',
-    padding: 8,
-    borderRadius: 30,
-    zIndex: 10,
+  scannerContainer: {
+    flex: 1,
+    // justifyContent: 'center',
+    alignItems: 'center',
+    paddingVertical: width * 0.2,
+    marginTop: 20,
+  },
+  scannerInnerContainer: {
+    width: SCAN_SIZE,
+    height: SCAN_SIZE,
+    borderRadius: 24,
+    borderColor: '#2337A8', // #2337A8
+    borderWidth: 4,
+    backgroundColor: '#000000', // #000000
+    overflow: 'hidden',
     alignItems: 'center',
     justifyContent: 'center',
-    width: 40,
-    height: 40,
   },
-  closeButtonText: {
-    color: 'white',
-    fontSize: 24,
+  infoBox: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 16,
+    paddingVertical: 8,
+    width: '100%',
+    justifyContent: 'center',
+    marginTop: 10,
   },
-  overlay: {
-    position: 'absolute',
-    backgroundColor: 'rgba(0, 0, 0, 0.6)',
+  infoIcon: {
+    marginRight: 8,
   },
-  scanRect: {
-    position: 'absolute',
-    borderWidth: 4,
-    borderColor: '#fff',
+  harmlessCard: {
+    borderRadius: 20,
+    backgroundColor: '#5DFFAE',
+    marginTop: 20,
+    padding: 10,
   },
-  scanLineContainer: {
-    flex: 1,
-    position: 'relative',
+  maliciousCard: {
+    borderRadius: 20,
+    backgroundColor: '#FE3A39',
+    marginTop: 20,
+    padding: 10,
   },
-  scanLine: {
-    position: 'absolute',
-    left: 0,
-    right: 0,
-    height: 2,
-    backgroundColor: 'blue',
+  chartContainer: {
+    borderRadius: 10,
+    backgroundColor: '#fff',
+    marginTop: 20,
   },
 });
-
-export default ScanQR;
