3d447268e2eb72c52cc63de64048c71da32c2b25
codebudy5247 <a.shekhar5247@gmail.com>
2025-04-25 23:07:12 +0530

    update api uri

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,6 +1,6 @@
 import { Api } from "./api";
 
-const baseURL = "https://322f-2001-df0-4940-940c-25e7-955f-2797-4801.ngrok-free.app/api/v1"
+const baseURL = "https://mcs-backend-754178159803.asia-south2.run.app/api/v1"
 
 // https://cybersafe-951156804825.asia-south1.run.app/api/v1
 
